d4c7b4e00d80ac5319d011318d0d90f52b776563
Shutsen <vancampenhoutgeert@gmail.com>
2018-10-15 21:24:16 +0200

    Basic setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mustacheExpress = require('mustache-express')
+require('dotenv').config()
+
+const app = express()
+
+const mustache = mustacheExpress()
+mustache.cache = null
+app.engine('mustache', mustache)
+app.set('view engine', 'mustache')
+
+app.use(express.static('public'))
+
+app.get('/list', (req, res) => {
+  res.render('list')
+})
+
+app.get('/movie-form', (req, res) => {
+  res.render('movie-form')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
+})
